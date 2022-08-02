7a8088ec4947299e37dadaa492cc0805d9e667f0
Anton Vanjukhin <frontbaze@gmail.com>
2022-08-02 13:30:43 +1000

    Add show hide modal

--- js/modal.js
+++ js/modal.js
@@ -1,2 +1,14 @@
 const modal = document.querySelector(".modal");
+const modalButton = document.querySelector(".contacts__button");
+const modalClose = document.querySelector(".modal__close");
+
+modalButton.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    modal.classList.add("show-modal");
+})
+
+modalClose.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    modal.classList.remove("show-modal");
+})
 
